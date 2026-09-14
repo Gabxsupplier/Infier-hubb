38140975ad913217e517406a663b2ede54c0fc83
Gabxsupplier <davidtransrapidos@gmail.com>
2026-09-13 23:45:21 -0600

    Infier hub

---
+++ chino-loadstring.lua
@@ -0,0 +1,4 @@
+-- CHINOINFIERNO | LOADSTRING
+-- Reemplaza la URL por la URL RAW de tu archivo chino-infierno-no-key.lua
+
+loadstring(game:HttpGet("https://TU-URL-RAW-AQUI/chino-infierno-no-key.lua"))()
